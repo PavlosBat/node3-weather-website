2c59b0fb9e2512f34b1de07121c6c46b570818d4
PavlosB <batsosp@yahoo.com>
2022-12-12 19:20:51 +0200

    Unnecessary log message

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,7 @@
 
+
+
+
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
@@ -15,7 +18,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent = 'Loading forecast...'
     messageTwo.textContent = ''
 
-    fetch('/weather?address=' + location).then((response) => {
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 messageOne.textContent = data.error
